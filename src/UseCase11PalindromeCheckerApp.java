56d5b6cca95a0850ce40ba77e30cf1d26651aba9
Krishn Raj <rajkrishn714@gmail.com>
2026-02-27 09:15:36 +0530

    Implemented UC11 Object Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,47 @@
+// MAIN CLASS
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        // Create service object
+        PalindromeService service = new PalindromeService();
+
+        // Call method
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+
+/**
+ * Service class that contains palindrome logic
+ */
+class PalindromeService {
+
+    /**
+     * Checks whether a given string is a palindrome
+     * @param input input string
+     * @return true if palindrome, otherwise false
+     */
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        // Compare characters moving inward
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
